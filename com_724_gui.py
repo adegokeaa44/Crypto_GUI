781dd8849a3f2d2be242db866f52e49580e9c38a
Adewale Alex Adegoke <115806599+adegokeaa44@users.noreply.github.com>
2023-05-04 22:16:43 +0100

    Update com_724_gui.py

--- com_724_gui.py
+++ com_724_gui.py
@@ -38,7 +38,7 @@ duration_type_list = ["day", "week", "month", "quarter"]
 duration_map = {"day": 1, "week": 7, "month": 30, "quarter": 90}
 models_list = ["Random Forest"]
 
-st.title("SOLiGence Real-Time Cryptocurrency Dashboard")
+st.title("FEMiGence Crypto Trend Live Dashboard")
 
 currency_filter = st.selectbox("Select the Currency", tickers_list)
 
